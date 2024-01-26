84c3bc6f3f7d8497cf589bfdd306389bdebf9ace
lihuiba <huiba.lhb@alibaba-inc.com>
2024-01-26 18:19:01 +0800

    fix

---
+++ net/http/test/to_url.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <photon/common/estring.h>
+#include <photon/net/socket.h>
+
+inline estring to_url(photon::net::ISocketServer* svr, std::string_view path) {
+    return estring().appends("localhost:", svr->getsockname().port, path);
+}
+
