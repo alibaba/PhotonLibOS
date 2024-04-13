af6635eab3f261a6bf3f52c75d42ee10740a95b0
Bob Chen <beef9999@qq.com>
2024-04-13 12:30:40 +0800

    update doc

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'PhotonLibOS',
-  tagline: 'Probably the fastest coroutine lib of the world',
+  tagline: 'Probably the fastest coroutine lib in the world!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
