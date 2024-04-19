cee149961c607dbb945b5113f4248a74ae2b53c3
lihuiba <huiba.lhb@alibaba-inc.com>
2024-04-19 12:14:42 +0800

    fix

--- test/gtest.h
+++ test/gtest.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #pragma GCC diagnostic push
-#ifdef __clang__
-#pragma GCC diagnostic ignored "-sign-compare"
-#endif
+// #ifdef __clang__
+#pragma GCC diagnostic ignored "-Wsign-compare"
+// #endif
 #include <gtest/gtest.h>
 #include <gtest/gtest-spi.h>
 #pragma GCC diagnostic pop
