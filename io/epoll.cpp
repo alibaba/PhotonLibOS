8aefb112ed836c0a0d4732d7748759db1f47ad10
Coldwings <coldwings@me.com>
2024-09-09 15:57:11 +0800

    FIX: wait_for_fd in epoll should atleast put into sleep for a tiny moment

--- io/epoll.cpp
+++ io/epoll.cpp
@@ -269,7 +269,9 @@ public:
         Event event{fd, interests | ONE_SHOT, CURRENT};
         int ret = add_interest(event);
         if (ret < 0) LOG_ERROR_RETURN(0, -1, "failed to add event interest");
-        ret = thread_usleep(timeout);
+        // if timeout is just simple 0, wait for a tiny little moment
+        // so that events can be collect.
+        ret = thread_usleep(timeout.timeout() ? timeout : Timeout(10));
         ERRNO err;
         if (ret == -1 && err.no == EOK) {
             return 0;  // Event arrived
