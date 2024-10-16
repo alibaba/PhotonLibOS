c5d661371683ddd6ddc146cab85f8bc0bd2e0956
lihuiba <huiba.lhb@alibaba-inc.com>
2024-10-16 17:59:02 +0800

    string_socket_stream

--- common/memory-stream/memory-stream.h
+++ common/memory-stream/memory-stream.h
@@ -16,7 +16,10 @@ limitations under the License.
 
 #pragma once
 #include <cinttypes>
+#include <string>
 #include <photon/common/stream.h>
+#include <photon/net/socket.h>
+#include <photon/common/string_view.h>
 
 
 class DuplexMemoryStream;
@@ -33,6 +36,9 @@ extern "C" DuplexMemoryStream* new_duplex_memory_stream(uint32_t capacity);
 // default is 11(3 in 10-based integer), both read and write operation may fail
 extern "C" IStream* new_fault_stream(IStream* stream, int flag=3, bool ownership=false);
 
+class StringSocketStream;
+StringSocketStream* new_string_socket_stream();
+
 class DuplexMemoryStream
 {
 public:
@@ -40,4 +46,25 @@ public:
     IStream* endpoint_a;     // do NOT delete it!!!
     IStream* endpoint_b;     // do NOT delete it!!!
     virtual int close() = 0;
+};
+
+class StringSocketStream : public photon::net::ISocketStream {
+protected:
+    std::string _in, _out;
+    std::string_view _inv;
+public:
+    void set_input(std::string_view in) {
+        _in.assign(in.data(), in.size());
+        _inv = {_in};
+    }
+    void set_input(std::string_view in, bool copy) {
+        if (!copy) _inv = in;
+        else set_input(in);
+    }
+    std::string_view input() {
+        return _inv;
+    }
+    std::string& output() {
+        return _out;
+    }
 };
