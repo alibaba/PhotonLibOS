92d4e3e283c45829f135458fcc0bb8a7ad6a5985
lihuiba <huiba.lhb@alibaba-inc.com>
2025-06-06 16:26:10 +0800

    make simple dom preseving the nodes' order of the document, by creating and sorting an index, instead of the nodes themselves.

--- ecosystem/simple_dom_impl.h
+++ ecosystem/simple_dom_impl.h
@@ -37,9 +37,10 @@ using str = estring_view;
 class NodeImpl : public Object {
 protected:
     NodeImpl() = default;
-    const static uint8_t FLAG_IS_ROOT       = 1;
-    const static uint8_t FLAG_IS_LAST       = 2;
-    const static uint8_t FLAG_TEXT_OWNERSHIP= 4;
+    const static uint8_t FLAG_IS_ROOT       = 1;    // the root node
+    const static uint8_t FLAG_IS_LAST       = 2;    // last sibling node
+    const static uint8_t FLAG_EQUAL_KEY_LAST= 4;    // last node with same key
+    const static uint8_t FLAG_TEXT_OWNERSHIP= 8;
     const static size_t  MAX_NODE_SIZE      = 256;
     const static size_t  MAX_NCHILDREN      = UINT16_MAX;
     const static size_t  MAX_KEY_OFFSET     = UINT32_MAX;
@@ -53,19 +54,17 @@ union { struct  { // for non-root nodes
         uint16_t _k_len : 12;       // key length (12 bits)
         uint16_t _v_off : 12;       // value offset (12 bits) to key end
     }__attribute__((packed));
-    uint32_t _k_off;                // key offset to _text_begin
     const NodeImpl* _root;          // root node
-    uint32_t _v_len;                // value length
-}                       ;           // packed as 20 bytes
+};                                  // packed as 20 bytes
 struct {         // for the root node
     uint8_t _flags_;                // the same as _flags
     uint8_t _node_size;             // sizeof(the node implementation)
     mutable uint16_t _refcnt;       // reference counter of the document
-    uint32_t _k_off_;
     const char* _text_begin;
-    uint32_t _v_len_;
 }; };
-    uint16_t _nchildren;            // for all nodes
+    uint32_t _k_off;                // key offset to _text_begin
+    uint32_t _v_len;                // value length
+    uint32_t _nchildren;            // for all nodes
 
     using AT16 = std::atomic<uint16_t>;
     static_assert(sizeof(AT16) == sizeof(_refcnt), "...");
@@ -107,10 +106,10 @@ public:
         return is_root() ? this : _root;
     }
     str get_key() const {
-        return {get_root()->_text_begin + _k_off, _k_len};
+        return get_key(get_root()->_text_begin);
     }
     str get_value() const {
-        return {get_key().end() + _v_off, _v_len};
+        return get_value(get_root()->_text_begin);
     }
     str get_key(const char* text_begin) const {
         return {text_begin + _k_off, _k_len};
