d688fb9c179b1ac720394c3c0eda6813cc006df2
Huiba Li <huiba.lhb@alibaba-inc.com>
2025-09-02 10:38:34 +0800

    make workpool's dtor consume less cpu if there's photon env (#969)

--- thread/workerpool.cpp
+++ thread/workerpool.cpp
@@ -45,6 +45,7 @@ public:
     int mode;
 
     impl(size_t vcpu_num, int ev_engine, int io_engine, int mode) : mode(mode) {
+        vcpus.reserve(vcpu_num);
         for (size_t i = 0; i < vcpu_num; ++i) {
             owned_std_threads.emplace_back(
                 &WorkPool::impl::worker_thread_routine, this, ev_engine,
@@ -53,10 +54,14 @@ public:
         ready_vcpu.wait(vcpu_num);
     }
 
-    ~impl() { // avoid depending on photon to make it destructible wihout photon
+    ~impl() {
         for (auto num = vcpus.size(); num; --num) enqueue({});
         for (auto &worker : owned_std_threads) worker.join();
-        while (vcpus.size()) std::this_thread::yield();
+        if (likely(CURRENT)) {
+            while (vcpus.size()) thread_yield();
+        } else {
+            while (vcpus.size()) std::this_thread::yield();
+        }
     }
 
     void enqueue(Delegate<void> call, AutoContext = {}) {
