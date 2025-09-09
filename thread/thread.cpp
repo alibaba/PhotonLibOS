bf5129701b7dcb656538bf36284a638c47d77f97
lihuiba <huiba.lhb@alibaba-inc.com>
2025-09-09 22:05:37 +0800

    make thread_yield_to() less prone to starve threads

--- thread/thread.cpp
+++ thread/thread.cpp
@@ -607,6 +607,8 @@ namespace photon
         }
         Switch try_goto(thread* th) const {
             assert(th->vcpu == vcpu);
+            th->remove_from_list();
+            current->insert_after(th);
             return _do_goto(th);
         }
         bool single() const {
@@ -1205,6 +1207,8 @@ R"(
             assert(th->state == states::READY);
         } else if (unlikely(th->state != states::READY)) {
             LOG_ERROR_RETURN(EINVAL, , "target thread ` must be READY!", th);
+        } else if (unlikely(rq.current->next() == th)) {
+            return thread_yield();
         }
 
         auto sw = AtomicRunQ(rq).try_goto(th);
