f2c291901c695d9d234b30f4aa2ad091067de1ed
PhotonLibOS <140932936+photonlibos@users.noreply.github.com>
2025-10-10 11:04:50 +0800

    fix iovector not checking size before extracting continuous buffer (#1010) (#1011) (#1013)
    * fix iovec not checking size before extracting continuous buffer
    
    Co-authored-by: NewbieOrange <NewbieOrange@users.noreply.github.com>

--- common/iovector.h
+++ common/iovector.h
@@ -535,12 +535,15 @@ public:
             update(va);
             return ptr;
         }
-
+        if (va.sum() < bytes) {
+            return nullptr;
+        }
+        
         auto buf = do_malloc(bytes);
-        auto ret = extract_front(bytes, buf);
-        return ret == bytes ?
-            buf :
-            nullptr;
+        if (buf) {
+            extract_front(bytes, buf);
+        }
+        return buf;
     }
 
     // try to extract `bytes` bytes from the back
@@ -626,12 +629,15 @@ public:
             update(va);
             return ptr;
         }
+        if (va.sum() < bytes) {
+            return nullptr;
+        }
 
         auto buf = do_malloc(bytes);
-        auto ret = extract_back(bytes, buf);
-        return ret == bytes ?
-            buf :
-            nullptr;
+        if (buf) {
+            extract_back(bytes, buf);
+        }
+        return buf;
     }
 
     // copy data to a buffer of `size` bytes,
