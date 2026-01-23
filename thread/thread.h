014c59a45fd2b6cd6271f34e5bd436f5076f86a8
Ian.han <jackinwhat@gmail.com>
2026-01-23 15:50:22 +0800

    Improve SCOPE_MAKESURE_YIELD macro (#1110)

--- thread/thread.h
+++ thread/thread.h
@@ -599,7 +599,7 @@ namespace photon
 */
 #define WITH_LOCK(mutex) if (auto __lock__ = scoped_lock(mutex))
 
-#define SCOPE_MAKESURE_YIELD                                               \
-    uint64_t __swc_##__LINE__ = get_vcpu(photon::CURRENT)->switch_count;   \
-    DEFER(if (__swc_##__LINE__ == get_vcpu(photon::CURRENT)->switch_count) \
-              photon::thread_yield(););
+#define SCOPE_MAKESURE_YIELD                                                              \
+    uint64_t _CONCAT(__swc__, __LINE__) = get_vcpu(photon::CURRENT)->switch_count;        \
+    DEFER(({if (_CONCAT(__swc__, __LINE__) == get_vcpu(photon::CURRENT)->switch_count)    \
+              photon::thread_yield();}))
