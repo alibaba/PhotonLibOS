9d103fc82ea66d547d2fbbe5937d320c69fd9dde
Huiba Li <huiba.lhb@alibaba-inc.com>
2026-05-19 11:43:40 +0800

    fix TEST(ThreadPool, migrate) (#1225)

--- thread/test/test-pool.cpp
+++ thread/test/test-pool.cpp
@@ -40,19 +40,19 @@ TEST(ThreadPool, test)
     // LOG_INFO("???????????????");
 }
 
-TEST(ThreadPool, migrate) {
+TEST(workpool, migrate) {
     WorkPool wp(4, 0, 0, -1);
-    ThreadPool<64> pool(64 * 1024);
-    vector<TPControl*> ths;
+    vector<photon::thread*> ths;
     ths.resize(FLAGS_ths_total);
     for (int i = 0; i < FLAGS_ths_total; i++) {
-        ths[i] = pool.thread_create_ex(&::func1, nullptr, true);
-        wp.thread_migrate(ths[i]->th);
+        ths[i] = thread_create(&::func1, nullptr);
+        thread_enable_join(ths[i]);
+        wp.thread_migrate(ths[i]);
     }
     LOG_INFO("----------");
     for (int i = 0; i < FLAGS_ths_total; i++) {
-        LOG_DEBUG("wait thread: `", ths[i]->th);
-        pool.join(ths[i]);
+        LOG_DEBUG("wait thread: `", ths[i]);
+        thread_join((join_handle*)ths[i]);
     }
     LOG_INFO("???????????????");
 }
