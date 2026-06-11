1e2591ae043e7b7c5913f5355fbbaf775709caef
水芝 <fengjiangtian.fjt@alibaba-inc.com>
2026-06-11 21:53:51 +0800

    Address review: move wc calculation into branches
    - Only clone/shrink iovec in overflow path
    - Calculate wc separately in each branch to avoid extra branching
    - Fixes sign comparison warning

--- net/http/body.cpp
+++ net/http/body.cpp
@@ -251,18 +251,19 @@ public:
 
     virtual ssize_t writev(const struct iovec *iov, int iovcnt) override {
         auto count = iovector_view((struct iovec*)iov, iovcnt).sum();
-        ssize_t wc = std::min(count, m_size - m_cnt);
         if (m_cnt + count > m_size) {
             LOG_WARN("data size overflow");
+            ssize_t wc = m_size - m_cnt;
             SmartCloneIOV<32> ciovec(iov, iovcnt);
             iovector_view view(ciovec.ptr, iovcnt);
             view.shrink_to(wc);
             if (m_stream->writev(view.iov, view.iovcnt) != wc) return -1;
-        } else {
-            if (m_stream->writev(iov, iovcnt) != wc) return -1;
+            m_cnt += wc;
+            return wc;
         }
-        m_cnt += wc;
-        return wc;
+        if (m_stream->writev(iov, iovcnt) != (ssize_t)count) return -1;
+        m_cnt += count;
+        return count;
     }
 
     virtual uint64_t timeout() const override {
