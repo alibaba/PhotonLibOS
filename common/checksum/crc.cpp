6658797bf0134d9240724a6e7247bed75ae51ae0
Huiba Li <huiba.lhb@alibaba-inc.com>
2026-07-03 21:10:31 +0800

    set x86-64 standard to v2; disable avx512 code for macOS; (#1526)

--- common/checksum/crc.cpp
+++ common/checksum/crc.cpp
@@ -635,6 +635,11 @@ uint64_t crc64ecma_hw_sse128(const uint8_t *buf, size_t len, uint64_t crc) {
 }
 
 #ifdef __x86_64__
+#ifdef __APPLE__ // apple basically doens't support avx-512
+uint64_t crc64ecma_hw_avx512(const uint8_t *buf, size_t len, uint64_t crc) {
+    return crc64ecma_hw_sse128(buf, len, crc);
+}
+#else
 #ifdef __clang__
 #pragma clang attribute pop
 #pragma clang attribute push (__attribute__((target("crc32,sse4.1,pclmul,avx512f,avx512dq,avx512vl,vpclmulqdq"))), apply_to=function)
@@ -735,6 +740,8 @@ uint64_t crc64ecma_hw_avx512(const uint8_t *buf, size_t len, uint64_t crc) {
 #else // __GNUC__
 #pragma GCC pop_options
 #endif
+
+#endif  // apple
 #endif  // __x86_64__
 
 // Pop aarch64 target options pushed at the beginning of the SIMD section
