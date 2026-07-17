bb3f09d62012b07a880008c79f17b692690acb13
PhotonLibOS <140932936+photonlibos@users.noreply.github.com>
2026-07-17 14:16:34 +0800

    add spill_additional_fp_simd_regs() (#1556) (#1557) (#1558)
    Co-authored-by: Huiba Li <huiba.lhb@alibaba-inc.com>

--- thread/thread.h
+++ thread/thread.h
@@ -453,6 +453,7 @@ namespace photon
     void* stackful_malloc(size_t size);
     void stackful_free(void* ptr);
 
+<<<<<<< HEAD
     // Set photon allocator/deallocator for photon thread stack
     // this is a hook for thread allocation, both alloc and dealloc
     // helps user to do more works like mark GC while allocating
@@ -492,6 +493,20 @@ namespace photon
       return z;
 #elif defined(__aarch64__)
       return x > y ? x - y : 0;
+=======
+    // Some additional registers are defined as callee-saved ones, such as
+    // d8 ~ d15 fp/simd registers in AARCH64, or xmm6 ~ xmm15 simd registers
+    // in Windows x64.
+    // But in fact, it is likely in reality that they do not actually need
+    // spilling, so we leave them here for the users to manually do spilling
+    // if needed.
+    inline void spill_additional_fp_simd_regs() {
+#ifdef __aarch64__
+        asm volatile("" ::: "d8", "d9", "d10", "d11", "d12", "d13", "d14", "d15");
+#elif defined(_WIN32)
+        asm volatile("" ::: "xmm6", "xmm7", "xmm8", "xmm9", "xmm10", "xmm11",
+                                          "xmm12", "xmm13", "xmm14", "xmm15");
+>>>>>>> b7edc80 (add spill_additional_fp_simd_regs() (#1556) (#1557) (#1558))
 #endif
     }
 };
